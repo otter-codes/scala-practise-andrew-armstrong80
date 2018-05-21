1e73d85aadf3002cd676e53c2d8b2be331307086
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-05-21 16:18:00 +0100

    12 tests passed

--- src/main/scala/RomanNumeralConverter.scala
+++ src/main/scala/RomanNumeralConverter.scala
@@ -1,17 +1,30 @@
+import scala.annotation.tailrec
+
 object RomanNumeralConverter {
 
-  def NumeralConverter(numeral : String) : Int = {
+  def NumeralConverter(numeral : Char) : Int = {
 
     numeral match {
 
-      case "I" => 1
-      case "X" => 10
-      case "C" => 100
-      case "M" => 1000
-      case "V" => 5
-      case "VII" => 7
-      case "L" => 50
-      case "D" => 500
+      case 'I' => 1
+      case 'X' => 10
+      case 'C' => 100
+      case 'M' => 1000
+      case 'V' => 5
+      case 'L' => 50
+      case 'D' => 500
     }
   }
+    def NumeralAdder(input : String) : Int = {
+
+      val subtract = input.toList.map(NumeralConverter).head
+      val prime = input.toList.map(NumeralConverter).tail.head
+
+      if (prime > subtract) {
+        prime - subtract
+      } else {
+        input.map(NumeralConverter).sum
+      }
+
+    }
 }
--- src/test/scala/RomanNumeralConverterSpec.scala
+++ src/test/scala/RomanNumeralConverterSpec.scala
@@ -5,37 +5,59 @@ class RomanNumeralConverterSpec extends WordSpec with MustMatchers {
   "NumeralConverter" must {
 
     "return 1 when given 'I'" in {
-      RomanNumeralConverter.NumeralConverter("I") mustEqual 1
+      RomanNumeralConverter.NumeralConverter('I') mustEqual 1
     }
 
 
     "return 10 when given X" in {
-      RomanNumeralConverter.NumeralConverter("X") mustEqual 10
+      RomanNumeralConverter.NumeralConverter('X') mustEqual 10
     }
 
     "return 100 when given C" in {
-      RomanNumeralConverter.NumeralConverter("C") mustEqual 100
+      RomanNumeralConverter.NumeralConverter('C') mustEqual 100
     }
 
     "return 10 when given M" in {
-      RomanNumeralConverter.NumeralConverter("M") mustEqual 1000
+      RomanNumeralConverter.NumeralConverter('M') mustEqual 1000
     }
 
     "return 5 when given V" in {
-      RomanNumeralConverter.NumeralConverter("V") mustEqual 5
-    }
-
-    "return 7 when given VII" in {
-      RomanNumeralConverter.NumeralConverter("VII") mustEqual 7
+      RomanNumeralConverter.NumeralConverter('V') mustEqual 5
     }
 
     "return 50 when given L" in {
-      RomanNumeralConverter.NumeralConverter("L") mustEqual 50
+      RomanNumeralConverter.NumeralConverter('L') mustEqual 50
     }
 
     "return 500 when given D" in {
-      RomanNumeralConverter.NumeralConverter("D") mustEqual 500
+      RomanNumeralConverter.NumeralConverter('D') mustEqual 500
     }
 
+  }
+
+  "NumeralAdder" must {
+
+    "return 12 when given XII" in {
+      RomanNumeralConverter.NumeralAdder("XII") mustEqual 12
+    }
+  }
+  "return 7 when given VII" in {
+    RomanNumeralConverter.NumeralAdder("VII") mustEqual 7
+  }
+
+    "return 4 when given IV" in {
+      RomanNumeralConverter.NumeralAdder("IV") mustEqual 4
+    }
+
+    "return 40 when given XL" in {
+      RomanNumeralConverter.NumeralAdder("XL") mustEqual 40
+    }
+
+  "return 1550 when given MDL" in {
+    RomanNumeralConverter.NumeralAdder("MDL") mustEqual 1550
+  }
+
+  "return 1984 when given MCMLXXXIV" ignore {
+    RomanNumeralConverter.NumeralAdder("MCMLXXXIV") mustEqual 1984
   }
 }
